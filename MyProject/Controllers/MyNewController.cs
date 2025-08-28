68aeb9dd4103d46a66a8c7b625c32b8ae5892616
LeLinh05 <linh83014@gmail.com>
2025-08-28 17:10:46 +0700

    First commit

---
+++ MyProject/Controllers/MyNewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
